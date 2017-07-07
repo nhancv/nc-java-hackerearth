100a9624a4676f2f2eacf84923da11df49cb95b6
Nhan Cao <nhancv92@gmail.com>
2017-07-07 11:28:08 +0700

    java buffered

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,14 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class TestClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(bufferedReader.readLine());
+
         int[] digits = new int[n];
         for (int i = 0; i < n; i++) {
-            digits[i] = scanner.nextInt();
+            digits[i] = Integer.valueOf(bufferedReader.readLine());
         }
         for (int i = n - 1; i > -1; i--) {
             System.out.println(digits[i]);
