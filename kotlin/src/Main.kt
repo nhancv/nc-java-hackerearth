6184044ec15a827f411b31f660e8229a7ed8efa4
Nhan Cao <nhancv92@gmail.com>
2017-07-07 10:57:15 +0700

    kotlin: input, array for loop

--- kotlin/src/Main.kt
+++ kotlin/src/Main.kt
@@ -3,6 +3,13 @@
  */
 
 fun main(args: Array<String>) {
-    println("Line: ${readLine()}")
-    println("Hello, World!")
+    val n: Int = readLine()!!.toInt()
+    val inputArr = Array(n, { i: Int -> i })
+    for (i in 0..n - 1) {
+        inputArr[i] = readLine()!!.toInt()
+    }
+
+    for (item in inputArr.reversedArray())
+        println(item)
+
 }
