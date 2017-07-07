dc5750b8c06168ca378c35a30eccd58a150c8f54
Nhan Cao <nhancv92@gmail.com>
2017-07-07 09:48:18 +0700

    add kotlin

---
+++ kotlin/src/Main.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by nhancao on 7/7/17.
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
+}
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        int[] digits = new int[10];
+        for (int i = 0; i < s.length(); i++) {
+            digits[s.charAt(i) - 48]++;
+        }
+        for (int i = 0; i < digits.length; i++) {
+            System.out.println(i + " " + digits[i]);
+        }
+
     }
 }
