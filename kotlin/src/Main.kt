dac1c4241787044c9540c152e72345f79b5a8aa6
Nhan Cao <nhancv92@gmail.com>
2017-07-10 10:08:38 +0700

    kotlin arr params

--- kotlin/src/Main.kt
+++ kotlin/src/Main.kt
@@ -2,12 +2,31 @@
  * Created by nhancao on 7/7/17.
  */
 
-fun main(args: Array<String>) {
-    val t: Int = readLine()!!.toInt()
-    for (i in 1..t) {
-        val n: Int = readLine()!!.toInt()
-        val s: String = java.lang.Integer.toBinaryString(n)
-        println(s.replace("0", "").length)
+fun queen(b: Array<Array<Int>>) {
+
+    for (i in 0..b.size - 1) {
+        for (j in 0..b[i].size - 1) {
+            println("${i} ${j} ${b[i][j]}")
+        }
     }
+
+}
+
+fun main(args: Array<String>) {
+//    val n: Int = readLine()!!.toInt()
+//
+//    val inputArr = IntArray(n)
+//    for (i in 0..n - 1) {
+//        inputArr[i] = readLine()!!.toInt()
+//    }
+
+    val arr = Array(11, { Array(11, { i: Int -> i }) })
+    for (i in 0..arr.size - 1) {
+        for (j in 0..arr[i].size - 1) {
+            arr[i][j] = (i + j)
+        }
+    }
+    queen(arr)
+
 
 }
