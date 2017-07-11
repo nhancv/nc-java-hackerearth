dbe1cf5a0278a3243f838466898cd33166454029
Nhan Cao <nhancv92@gmail.com>
2017-07-11 11:56:16 +0700

    Print hackerearth

--- kotlin/src/Main.kt
+++ kotlin/src/Main.kt
@@ -2,62 +2,35 @@
  * Created by nhancao on 7/7/17.
  */
 
-fun isAttacked(x: Int, y: Int, board: Array<Array<Int>>): Boolean {
-    for (i in 0..board.size - 1) {
-        if (board[x][i] == 1) return true
-    }
-
-    for (i in 0..board.size - 1) {
-        if (board[i][y] == 1) return true
-    }
-
-    for (i in maxOf(x, y) downTo 0) {
-        if (x - i >= 0 && y - i >= 0 && board[x - i][y - i] == 1) return true
-        if (x - i >= 0 && y + i < board.size && board[x - i][y + i] == 1) return true
-        if (x + i < board.size && y + i < board.size && board[x + i][y + i] == 1) return true
-        if (x + i < board.size && y - i >= 0 && board[x + i][y - i] == 1) return true
-    }
-
-    for (i in 0..board.size - 1 - minOf(x, y)) {
-        if (x - i >= 0 && y - i >= 0 && board[x - i][y - i] == 1) return true
-        if (x - i >= 0 && y + i < board.size && board[x - i][y + i] == 1) return true
-        if (x + i < board.size && y + i < board.size && board[x + i][y + i] == 1) return true
-        if (x + i < board.size && y - i >= 0 && board[x + i][y - i] == 1) return true
-    }
-
-    return false
-
-}
-
-fun queen(board: Array<Array<Int>>, n: Int): Boolean {
-    if (n == 0) return true
-
-    for (i in 0..board.size - 1) {
-        for (j in 0..board.size - 1) {
-            if (isAttacked(i, j, board)) continue
-            board[i][j] = 1
-            if (queen(board, n - 1)) return true
-            board[i][j] = 0
-        }
-    }
-    return false
-}
-
 fun main(args: Array<String>) {
     val n: Int = readLine()!!.toInt()
+    val s: String = readLine()!!
 
-    val arr = Array(n, { Array(n, { 0 }) })
+    var counth = 0 //x2
+    var counta = 0 //x2
+    var countc = 0
+    var countk = 0
+    var counte = 0 //x2
+    var countr = 0 //x2
+    var countt = 0
 
-    if (queen(arr, n)) {
-        println("YES")
-        for (i in 0..n - 1) {
-            val s: StringBuilder = StringBuilder()
-            for (j in 0..n - 1) {
-                s.append("${arr[i][j]} ")
-            }
-            println(s)
-        }
-    } else println("NO")
+    for (i in 0..s.length - 1) {
+        if (s[i] == 'h') counth++
+        if (s[i] == 'a') counta++
+        if (s[i] == 'c') countc++
+        if (s[i] == 'k') countk++
+        if (s[i] == 'e') counte++
+        if (s[i] == 'r') countr++
+        if (s[i] == 't') countt++
+    }
+
+    counth /= 2
+    counta /= 2
+    counte /= 2
+    countr /= 2
+
+    var min = minOf(countr, countt, minOf(countk, counte, minOf(counth, counta, countc)))
+    println(min)
 
 
 }
