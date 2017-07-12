b54de6ee637f1bf5c9f541a7bba3477b57b3168e
Nhan Cao <nhancv92@gmail.com>
2017-07-12 14:30:40 +0700

    input array

--- kotlin/src/Main.kt
+++ kotlin/src/Main.kt
@@ -3,34 +3,23 @@
  */
 
 fun main(args: Array<String>) {
-    val n: Int = readLine()!!.toInt()
-    val s: String = readLine()!!
+    val (n, m) = readLine()!!.split(" ")
 
-    var counth = 0 //x2
-    var counta = 0 //x2
-    var countc = 0
-    var countk = 0
-    var counte = 0 //x2
-    var countr = 0 //x2
-    var countt = 0
+    val arr = Array(n.toInt(), { Array(m.toInt(), { 0 }) })
 
-    for (i in 0..s.length - 1) {
-        if (s[i] == 'h') counth++
-        if (s[i] == 'a') counta++
-        if (s[i] == 'c') countc++
-        if (s[i] == 'k') countk++
-        if (s[i] == 'e') counte++
-        if (s[i] == 'r') countr++
-        if (s[i] == 't') countt++
+    for (i in 0..n.toInt() - 1) {
+        val tmp = readLine()!!.split(" ")
+        for (j in 0..m.toInt() - 1) {
+            arr[i][j] = tmp[j].toInt()
+        }
     }
 
-    counth /= 2
-    counta /= 2
-    counte /= 2
-    countr /= 2
-
-    var min = minOf(countr, countt, minOf(countk, counte, minOf(counth, counta, countc)))
-    println(min)
-
+    for (i in 0..m.toInt() - 1) {
+        var s = ""
+        for (j in 0..n.toInt() - 1) {
+            s += (arr[j][i].toString() + " ")
+        }
+        println(s)
+    }
 
 }
